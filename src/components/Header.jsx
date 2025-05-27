53f053dc6024d35b28b6e8e0ee11e4eb9d4d49f0
Shaun Burley <shaunburley@UIPL1277-MacBook-Pro.local>
2025-05-27 14:51:22 -0700

    changed header background

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,12 +8,12 @@ export default function Header() {
   const bgImage = '/images/header-bg.png';
 
   return (
-    <header className="hidden md:block relative text-center py-10 bg-cover bg-center shadow-lg"
+    <header className="hidden md:block relative text-center py-10 bg-repeat shadow-lg"
       style={{ backgroundImage: `url(${bgImage})` }}
     >
       <div className="absolute inset-0 bg-white/35 backdrop-brightness-125 z-0" />
 
-      <div className="relative z-10 bg-[rgba(3,1,4,0.75)] backdrop-blur-xs inline-block px-6 py-4 rounded-4xl border-2 border-[var(--color-red)]">
+      <div className="relative z-10 bg-[rgba(3,1,4,0.75)] backdrop-blur-xs inline-block px-6 py-4 rounded-4xl border-2 border-[var(--color-cream-dark)]">
         <img
           src="/images/blue-rose.png"
           alt="blue rose"
